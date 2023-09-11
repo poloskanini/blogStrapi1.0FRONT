8cd19095e6306d1d58bcad4c184e3afb1376d9f8
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-09-11 08:08:34 +0200

    (bf-deploy) no <img> in header, and normal+italic styles in fonts

--- components/Header.js
+++ components/Header.js
@@ -159,12 +159,12 @@ export default function Header() {
             <Link
               href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">MENEZES AVOCAT</span>
-              <img
+              {/* <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt="Menezes Avocat"
                 onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-              />
+              /> */}
             </Link>
             <button
               type="button"
