f27eae5113efc8fac66624b5e64f18b45fcb0fad
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-09-13 09:49:24 +0200

    (ft-Index) SEO elementaire avec metas

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,21 @@ export default function Home() {
   return (
     <>
       <Head>
-          <title>Menezes Avocat</title>
+        <title>Menezes Avocat</title>
+        <meta name="title" content="Menezes Avocat" />
+        <meta name="description" content="Le cabinet MENEZES AVOCAT est à votre disposition sur Paris afin de vous conseiller et vous défendre dans toutes vos affaires juridiques liées au droit du travail et de la sécurité sociale." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="" />
+        <meta property="og:title" content="Menezes Avocat" />
+        <meta property="og:description" content="Le cabinet MENEZES AVOCAT est à votre disposition sur Paris afin de vous conseiller et vous défendre dans toutes vos affaires juridiques liées au droit du travail et de la sécurité sociale." />
+        <meta property="og:image" content="https://res.cloudinary.com/dbff7xgqx/image/upload/v1694591001/image-description-menezes_zvl0ti.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="" />
+        <meta property="twitter:title" content="Menezes Avocat" />
+        <meta property="twitter:description" content="Le cabinet MENEZES AVOCAT est à votre disposition sur Paris afin de vous conseiller et vous défendre dans toutes vos affaires juridiques liées au droit du travail et de la sécurité sociale." />
+        <meta property="twitter:image" content="https://res.cloudinary.com/dbff7xgqx/image/upload/v1694591001/image-description-menezes_zvl0ti.png" />
       </Head>
       
       <Layout />
