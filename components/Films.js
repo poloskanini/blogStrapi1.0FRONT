20a07b9d75025248bfbac1d1c6433192160ffd21
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-09-14 14:47:07 +0200

    (ft-Blog) Ajout d'un container pour la liste de films

--- components/Films.js
+++ components/Films.js
@@ -1,9 +1,11 @@
 import Link from "next/link"
+import Container from "./Container"
 
 export default function Films( {films} ) {
   
   return (
     <>
+    <Container>
       <ul className="list-none space-y-4 text-lg font-bold mt-10 h-80">
         {films &&
           films.data.map((film) => {
@@ -14,6 +16,7 @@ export default function Films( {films} ) {
             )
           })}
       </ul>
+    </Container>
     </>
   )
 }
