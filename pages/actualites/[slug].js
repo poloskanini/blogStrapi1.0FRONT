e57fde5e7b092826bbfbfaae0dd54a1358ccec92
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-09-19 18:42:30 +0200

    (bf-BreadCrumb) Ajout de chevrons manquants

--- pages/actualites/[slug].js
+++ pages/actualites/[slug].js
@@ -50,6 +50,7 @@ export default function Post({ post }) {
               </div>
             </li>
             <li className="flex">
+              <ChevronRightIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
               <Link
                 href={"/actualites"}
                 className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
