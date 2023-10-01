65aacc844326ac622eebec94464bf6f469a7ca5a
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-10-01 20:41:16 +0200

    (ft-Header) Changement de couleur du header + ajout de gifs Chandler waiting for client...

--- pages/honoraires.js
+++ pages/honoraires.js
@@ -28,6 +28,10 @@ export default function honoraires() {
         <br />
         <h2>Page en attente de wording et images</h2>
         <br />
+        
+        <iframe src="https://giphy.com/embed/kcfiK948yUHSqoNzwi" width="100%" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/Friends-episode-2-season-9-friendstv-kcfiK948yUHSqoNzwi"></a></p>
+
+        
 
         {/* <div className="wrapper-reveal flex justify-end ">
           <TextReveal className="relative" />
