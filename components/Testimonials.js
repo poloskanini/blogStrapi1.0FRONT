97ef7da9a62f0d8c55c4806e75986d7cff721b26
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-10-05 20:58:07 +0200

    (ft - Home) Ajout de Testimonials et Testify - (bf) Balise Image iso img

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -1,4 +1,6 @@
 import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image'
+
 
 const people = [
   { id: 1,
@@ -82,35 +84,35 @@ export default function Testimonials() {
               adipisicing officia excepteur tempor.
             </p>
             <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-              <img
+              <Image
                 className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
                 src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
                 alt="Transistor"
                 width={158}
                 height={48}
               />
-              <img
+              <Image
                 className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
                 src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
                 alt="Reform"
                 width={158}
                 height={48}
               />
-              <img
+              <Image
                 className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
                 src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
                 alt="Tuple"
                 width={158}
                 height={48}
               />
-              <img
+              <Image
                 className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
                 src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
                 alt="SavvyCal"
                 width={158}
                 height={48}
               />
-              <img
+              <Image
                 className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
                 src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
                 alt="Statamic"
