5e91f07216e442cca589973c604af7bd3427ee8f
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-10-17 12:37:38 +0200

    (ft-Loader Animation) Animation SVG du logo MENEZES au chargement du site- ajustement du loading time

--- pages/_app.js
+++ pages/_app.js
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }) {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 2800)
+    }, 2500)
   }, [])
   
   return (
