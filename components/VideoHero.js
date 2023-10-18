5a33aa3eb77c433140d15fa7d44fa667ba26027d
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-10-18 20:16:13 +0200

    (FACTORIZE-VideoHeader) Augmentation à 80% de brightness

--- components/VideoHero.js
+++ components/VideoHero.js
@@ -1,6 +1,6 @@
 export default function VideoHero() {
   return (
-    <div className="h-full w-full absolute z-0 object-cover brightness-70">
+    <div className="h-full w-full absolute z-0 object-cover brightness-80">
       <video className="h-[110vh] w-full object-cover wide:landscape:h-auto" autoPlay playsInline loop muted poster={'/assets/images/videoPoster3.webp'} src="/assets/videos/heroVideo2.mp4"></video>
     </div>
   )
