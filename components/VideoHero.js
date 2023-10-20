83f0d6269f554b64c2a165c626e94c0713ebb0eb
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-10-20 14:27:16 +0200

    (bf-VideoHero) Changement pour video-sm

--- components/VideoHero.js
+++ components/VideoHero.js
@@ -1,7 +1,7 @@
 export default function VideoHero() {
   return (
     <div className="h-full w-full absolute z-0 object-cover brightness-70">
-      <video className="h-[110vh] w-full object-cover wide:landscape:h-auto" autoPlay playsInline loop muted poster={'/assets/images/videoPoster3.webp'} src="/assets/videos/heroVideo2.mp4"></video>
+      <video className="h-[110vh] w-full object-cover wide:landscape:h-auto" autoPlay playsInline loop muted poster={'/assets/images/videoPoster3.webp'} src="/assets/videos/heroVideo-sm.mp4"></video>
     </div>
   )
 }
