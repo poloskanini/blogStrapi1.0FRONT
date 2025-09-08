9907748ed543726b17ca34f1206dacdc6879bf52
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2025-09-08 13:11:42 +0200

    (seo) canonical ajoutée dans _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head />
+      <Head>
+        <link rel="canonical" href="https://menezes-avocat.com/" />
+      </Head>
       <body>
         <Main />
         <NextScript />
